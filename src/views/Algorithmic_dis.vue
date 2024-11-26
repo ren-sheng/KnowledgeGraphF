88a9055493216f412e56856b4de48e131e29d532
Tide-sunrise <1834049428@qq.com>
2024-11-27 00:28:00 +0800

    合作者消歧图片展示

--- src/views/Algorithmic_dis.vue
+++ src/views/Algorithmic_dis.vue
@@ -28,36 +28,7 @@ const options = {
 onMounted(() => {
   //导入json文件数据,路径为src/assets/data/algorithmic_dis.json
   console.log(data)
-  // const jsonData = {
-  //   rootId: 'a',
-  //   nodes: [
-  //     { id: 'a', text: 'a', },
-  //     { id: 'b', text: 'b', },
-  //     { id: 'c', text: 'c', },
-  //     { id: 'd', text: 'd', },
-  //     { id: 'e', text: 'e', },
-  //     { id: 'f', text: 'f', },
-  //   ],
-  //   lines: [
-  //     { from: 'a', to: 'b', },
-  //     { from: 'a', to: 'c', },
-  //     { from: 'a', to: 'd', },
-  //     { from: 'a', to: 'e', },
-  //     { from: 'a', to: 'f', },
-  //   ],
-  // }
   console.log(data)
-  // The node and line in the above data can refer to the options in "Node" and "Link & Line" for configuration.
-  // Node: https://www.relation-graph.com/#/docs/node
-  // Link & Line: https://www.relation-graph.com/#/docs/link
   graphRef$.value.setJsonData(data)
-  // The graphRef$.value.setJsonData(jsonData, callback) method is a convenient method that is equivalent to the following code:
-  //  const graphInstance = graphRef$.value.getInstance();
-  //  graphInstance.addNodes(jsonData.nodes);
-  //  graphInstance.addLines(jsonData.lines);
-  //  graphInstance.rootNode = graphInstance.getNodeById(jsonData.rootId);
-  //  await graphInstance.doLayout(); // Layout using the layouter set in graphOptions
-  //  await graphInstance.moveToCenter(); // Find the center based on node distribution and center the view
-  //  await graphInstance.zoomToFit(); // Zoom to fit, so that all nodes can be displayed in the visible area
 })
 </script>
--- src/views/RegularDisambiguationCo.vue
+++ src/views/RegularDisambiguationCo.vue
@@ -1,5 +1,6 @@
 <template>
   <view class="layout">
+
     <view class="title">
       <text class="title-text">规则消歧展示</text>
     </view>
@@ -13,7 +14,7 @@
       </view>
       <view class="button">
         <!--        消歧按钮，置于右侧-->
-        <el-button type="primary" @click="ansco = true">合作者消歧</el-button>
+        <el-button type="primary" @click="graphOn">合作者消歧</el-button>
       </view>
       <view class="ansco" v-if="ansco">
         <text>根据合作者消歧</text>
@@ -22,19 +23,48 @@
           <el-table-column prop="AFTrue" label="AuthorFullName"/>
         </el-table>
         <!--        用relation-graph画出作者合作关系-->
-        <view class="relation-graph">
 
+      </view>
+      <view class="graph">
+        <view style="border: #efefef solid 1px; height: 60vh;width: 50%;">
+          <relation-graph ref="graphRef$" :options="options"/>
         </view>
       </view>
     </view>
   </view>
 </template>
+<script setup lang="ts">
+import {onMounted, ref} from 'vue'
+import RelationGraph from 'relation-graph-vue3'
+import data2 from '../resource/author_data.json'
 
-<script setup>
-import { ref } from 'vue'
+const graphRef$ = ref<RelationGraph>()
+const options = {
+  "backgroundImageNoRepeat": true,
+  "moveToCenterWhenRefresh": false,
+  "layouts": [
+    {
+      "label": "中心",
+      "layoutName": "center",
+      "centerOffset_x": 0,
+      "centerOffset_y": 0,
+      "distance_coefficient": 1
+    }
+  ]
+}
 
+interface TableDataItem {
+  AF: string;
+  combine: string; // 合作者
+}
 
-const tableData = ref([
+interface TableData2Item {
+  AFFalse: string;
+  AFTrue: string;
+}
+const ansco = ref<boolean>(false);
+
+const tableData = ref<TableDataItem[]>([
   {
     AF: "Emily Davis",
     combine: "John Smith, Alice Johnson" // 第一条记录的合作者
@@ -48,7 +78,8 @@ const tableData = ref([
     combine: "Michael Brown, Alice Johnson" // 第三条记录，与第二条记录的合作者相同
   }
 ]);
-const tableData2 = ref([
+
+const tableData2 = ref<TableData2Item[]>([
   {
     AFFalse: "*",
     AFTrue: "Emily Davis"
@@ -61,18 +92,30 @@ const tableData2 = ref([
     AFFalse: "Emily davis",
     AFTrue: "Emily Davis"
   }
-])
+]);
 
-const ansid = ref(false);
-const ansco = ref(false);
+const graphOn = () => {
+  ansco.value = !ansco.value
+  graphRef$.value.setJsonData(data2)
+}
+onMounted(() => {
+  //导入json文件数据,路径为src/assets/data/algorithmic_dis.json
+  console.log(data2)
+
+})
 </script>
-
 <style scoped lang="scss">
 .layout{
   display: flex;
   flex-direction: column;
   align-items: center;
-
+  .graph{
+    width: 100%;
+    height: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
   //justify-content: center;
   .title{
     width: 100%;
